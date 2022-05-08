03ba61a0efbc21ba2d39dac851366f1811f132e7
O-Shaheen <oshaheen@aucegypt.edu>
2022-05-08 06:29:27 +0200

    Changes to be committed:  	modified:   backgroundRemover/virtualEnviroment/app.py  	modified:   backgroundRemover/virtualEnviroment/build/app/Analysis-00.toc  	modified:   backgroundRemover/virtualEnviroment/build/app/EXE-00.toc  	modified:   backgroundRemover/virtualEnviroment/build/app/PKG-00.toc  	modified:   backgroundRemover/virtualEnviroment/build/app/PYZ-00.pyz  	modified:   backgroundRemover/virtualEnviroment/build/app/PYZ-00.toc  	modified:   backgroundRemover/virtualEnviroment/build/app/app.pkg  	modified:   backgroundRemover/virtualEnviroment/build/app/warn-app.txt  	modified:   backgroundRemover/virtualEnviroment/build/app/xref-app.html  	modified:   backgroundRemover/virtualEnviroment/dist/app.exe  	deleted:    backgroundRemover/virtualEnviroment/dist/inputimages/im1.jpg  	deleted:    backgroundRemover/virtualEnviroment/dist/inputimages/im2.jpg  	deleted:    backgroundRemover/virtualEnviroment/dist/outputimages/im1.jpg  	deleted:    backgroundRemover/virtualEnviroment/dist/outputimages/im2.jpg  	deleted:    backgroundRemover/virtualEnviroment/outputimages/im1.jpg

--- backgroundRemover/virtualEnviroment/app.py
+++ backgroundRemover/virtualEnviroment/app.py
@@ -5,21 +5,24 @@ import requests
 #curDir = os.getcwd()
 
 #print(curDir)
-inputDir = 'inputimages/'
-outputDir = 'outputimages/'
+inputDir = '/'
+outputDir = 'outputimagesTemp/'
+os.mkdir(outputDir)
 from os import listdir
 api_key = input("Enter API Key:")
-for img in os.listdir(inputDir):
+for img in os.listdir():
+    #print(img)
     if (img.endswith(".png") or img.endswith(".jpg") or img.endswith(".jpeg")):
         img_path = os.path.join(os.path.realpath(inputDir), img)
+        #print(img_path)
         updated_outputDir = os.path.join(os.path.realpath(outputDir), img)
         #shutil.copy(img_path, updated_outputDir)
         dec = input(f"Shall I remove background for file {img} ?")
-        if(dec != 'N'):
+        if(dec.upper() != 'N'):
             print(f"removing {img} background")
             response = requests.post(
             'https://api.remove.bg/v1.0/removebg',
-            files={'image_file': open(img_path, 'rb')},
+            files={'image_file': open(img, 'rb')},
             data={'size': 'auto'},
             headers={'X-Api-Key': api_key},
             
@@ -27,11 +30,20 @@ for img in os.listdir(inputDir):
             if response.status_code == requests.codes.ok:
                 with open(img, 'wb') as out:
                     out.write(response.content)
-                shutil.move(img, updated_outputDir)    
+                shutil.move(img, updated_outputDir)
+                #os.remove(img)    
             else:
                 print("Error:", response.status_code, response.text)
+                os.rename(img, updated_outputDir)
         else:
             print(f"Will not remove {img} background")
+            os.rename(img, updated_outputDir)
+for img in os.listdir(outputDir):
+    img_path = os.path.join(os.path.realpath(outputDir), img)
+    updated_outputDir = os.path.join(os.getcwd(), img)
+    os.rename(img_path, updated_outputDir)
+os.rmdir('outputimagesTemp')    
+
 print(f"finished looping over all the images")
 
         #print(img_path)
